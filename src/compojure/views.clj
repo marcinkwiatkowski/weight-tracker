2de588db60eb3150df08a8850b04a6e5d31ca25d
Marcin Kwiatkowski <marcin.kwiatkowski@hotmail.com>
2012-10-15 13:21:20 +0100

    add twitter bootstrap

--- src/compojure/views.clj
+++ src/compojure/views.clj
@@ -4,7 +4,7 @@
 (defn index-page [weights]
   (html5
     [:head [:title "Weight Tracker"]
-     (include-css "/css/style.css")]
+     (include-css "/css/bootstrap-responsive.min.css", "/css/bootstrap.min.css")]
     [:body [:div {:id "holder"} (ordered-list weights)]
      [:h1 "Weight Tracker"]
      [:div (form-to [:post "/save"]
