8301abbc7b2cc9a9f50d5f068f5b9c4a2ec214c8
Marcin Kwiatkowski <marcin.kwiatkowski@hotmail.com>
2012-10-15 18:44:17 +0100

    wrap everything in container, remove h1 title

--- src/compojure/views.clj
+++ src/compojure/views.clj
@@ -5,11 +5,12 @@
   (html5
     [:head [:title "Weight Tracker"]
      (include-css "/css/bootstrap-responsive.min.css", "/css/bootstrap.min.css")]
-    [:body [:div {:id "holder"} (ordered-list weights)]
-     [:h1 "Weight Tracker"]
-     [:div (form-to [:post "/save"]
-       (label "weight" "Today's weight")
-       (text-field {:maxlength 4 :autofocus true} "weight"))]
+    [:body [:div {:class "container"}
+            [:div {:id "holder"} (ordered-list {:style "display: none"} weights)]
+            [:div (form-to [:post "/save"]
+              (label "weight" "Today's weight")
+              (text-field {:maxlength 4 :autofocus true} "weight"))]
+            ]
      [:script {:src "https://www.google.com/jsapi"}]
      [:script {:src "//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"}]
      (include-js "/javascript/script.js")
